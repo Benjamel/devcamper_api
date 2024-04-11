064792f7fa572643b5f50017f0685ec138b4263c
Benjamel <benjamin.meldal@gmail.com>
2024-04-11 08:24:39 +0200

    installed packages

--- server.js
+++ server.js
@@ -1,20 +1,38 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const logger = require('./middleware/logger');
-
-//Route files
-const bootcamps = require('./routes/bootcamps');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Connect to database
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
 
-app.use(logger);
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, console.log(`Server running ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+  PORT,
+  console.log(`Server running ${process.env.NODE_ENV} mode on port ${PORT}.`.yellow.bold)
+);
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
